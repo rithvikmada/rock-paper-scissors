e7abd2fa8b47f585f7f71bf0ebc682c138a6eeb7
Rithvik Mada <73190672+rithvikmada@users.noreply.github.com>
2024-07-08 19:07:32 -0400

    Get user choice from input (word or character)
    - Write logic for getUserChoice() to accept both word and character as
    input

--- script.js
+++ script.js
@@ -4,20 +4,34 @@ let cpuChoice;  // Initialize variable
 
 function getComputerChoice() {
     
-    let randomNum = Math.floor(Math.random() * 3);  // Get number between 0-3
+    let cpuNum = Math.floor(Math.random() * 3);  // Get number between 0-2
 
-    if (randomNum == 0) {   // Assign number to choice
-        cpuChoice = "Rock"
+    if (cpuNum == 0) {   // Assign number to choice
+        cpuChoice = "ROCK";
     }
-    else if (randomNum == 1) {
-        cpuChoice = "Paper"
+    else if (cpuNum == 1) {
+        cpuChoice = "PAPER";
     }
     else {
-        cpuChoice = "Scissors"
+        cpuChoice = "SCISSORS";
     }
 
 }
 
 // Get user's choice
+
+let userChoice;
+
+function getUserChoice() {
+
+    userChoice = (prompt("Choose Rock, Paper, or Scissors (word or first letter)")).toUpperCase(); 
+
+    if (userChoice == "R") userChoice = "ROCK";     // Logic for single-character entry
+    else if (userChoice == "P") userChoice = "PAPER";
+    else if (userChoice == "S") userChoice = "SCISSORS";
+    else userChoice = userChoice;
+
+}
+
 // Initialize score
 // Play rounds (best of 5), update score
