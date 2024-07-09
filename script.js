c8240562959b2c841503c461a6fad3e484b8f5f9
Rithvik Mada <73190672+rithvikmada@users.noreply.github.com>
2024-07-09 18:08:03 -0400

    Declare playRound() and build game loop logic

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Get computer's choice
 
-let cpuChoice;  // Initialize variable
+let cpuChoice;   // Initialize variable
 
 function getComputerChoice() {
     
-    let cpuNum = Math.floor(Math.random() * 3);  // Get number between 0-2
+    let cpuNum = Math.floor(Math.random() * 3);   // Get number between 0-2
 
-    if (cpuNum == 0) {   // Assign number to choice
+    if (cpuNum == 0) {    // Assign number to choice
         cpuChoice = "ROCK";
     }
     else if (cpuNum == 1) {
@@ -16,21 +16,24 @@ function getComputerChoice() {
         cpuChoice = "SCISSORS";
     }
 
+    return cpuChoice;
+
 }
 
 // Get user's choice
 
-let userChoice;
+let userChoice;   // Initialize variable
 
 function getUserChoice() {
 
     userChoice = (prompt("Choose Rock, Paper, or Scissors (word or first letter)")).toUpperCase(); 
 
-    if (userChoice == "R") userChoice = "ROCK";     // Logic for single-character entry
+    if (userChoice == "R") userChoice = "ROCK";   // Logic for single-character entry
     else if (userChoice == "P") userChoice = "PAPER";
     else if (userChoice == "S") userChoice = "SCISSORS";
     else userChoice = userChoice;
 
+    return userChoice;
 }
 
 // Initialize score
@@ -38,4 +41,21 @@ function getUserChoice() {
 let cpuScore = 0;
 let userScore = 0;
 
+while (cpuScore < 3 && userScore < 3) {
+    playRound(getComputerChoice(), getUserChoice());
+}
+
+(cpuScore == 3) 
+    ? console.log(`CPU wins the game ${cpuScore}-${userScore}`)
+    : console.log(`User wins the game ${userScore}-${cpuScore}`);
+
 // Play rounds (best of 5), update score
+
+// Check if either player has score of 3 
+//
+
+function playRound(cpu, user) {
+    console.log(cpu);
+    console.log(user)
+    cpuScore++;
+}
